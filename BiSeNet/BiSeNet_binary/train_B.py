7c79b2ce5ce1c172068d3f33e626879595346153
albertocavallo <75563684+albertocavallo@users.noreply.github.com>
2023-05-10 15:04:47 +0200

    Update train_B.py

--- BiSeNet/BiSeNet_binary/train_B.py
+++ BiSeNet/BiSeNet_binary/train_B.py
@@ -11,7 +11,7 @@ import torchvision.transforms as standard_transforms
 import torchvision.utils as vutils
 from tensorboardX import SummaryWriter
 from ptflops import get_model_complexity_info
-from bisenet_v1 import BiSeNet
+from model_B import BiSeNet
 from config import cfg
 from loading_data import loading_data
 from utils import *
@@ -25,41 +25,6 @@ writer = SummaryWriter(cfg.TRAIN.EXP_PATH+ '/' + exp_name)
 
 pil_to_tensor = standard_transforms.ToTensor()
 train_loader, val_loader, restore_transform = loading_data()
-
-
-def flatten(tensor):
-    """Flattens a given tensor such that the channel axis is first.
-    The shapes are transformed as follows:
-       (N, C, D, H, W) -> (C, N * D * H * W)
-    """
-    C = tensor.size(1)
-    # new axis order
-    axis_order = (1, 0) + tuple(range(2, tensor.dim()))
-    # Transpose: (N, C, D, H, W) -> (C, N, D, H, W)
-    transposed = tensor.permute(axis_order)
-    # Flatten: (C, N, D, H, W) -> (C, N * D * H * W)
-    return transposed.contiguous().view(C, -1)
-
-
-class DiceLoss(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.epsilon = 1e-5
-
-    def forward(self, output, target):
-        assert output.size() == target.size(), "'input' and 'target' must have the same shape"
-        output = F.softmax(output, dim=1)
-        output = flatten(output)
-        target = flatten(target)
-        # intersect = (output * target).sum(-1).sum() + self.epsilon
-        # denominator = ((output + target).sum(-1)).sum() + self.epsilon
-
-        intersect = (output * target).sum(-1)
-        denominator = (output + target).sum(-1)
-        dice = intersect / denominator
-        dice = torch.mean(dice)
-        return 1 - dice
-        # return 1 - 2. * intersect / denominator
 
 
 def bce_loss(pred, label):
