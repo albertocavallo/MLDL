bdf9a1c82bc8f0329772d03bcd3a74a0276ea6d7
deriddu <stefanochiartano2000@gmail.com>
2023-05-16 14:36:20 +0200

    Float -> Long for crossEntropyloss

--- ICNet/ICNet_instance/loss_I.py
+++ ICNet/ICNet_instance/loss_I.py
@@ -20,16 +20,15 @@ class ICNetLoss(nn.CrossEntropyLoss):
         preds, target = tuple(inputs)
         inputs = tuple(list(preds) + [target])
 
-        pred_sub4, pred_sub8, pred_sub16, target = tuple(inputs)
+        pred, pred_sub4, pred_sub8, pred_sub16, target = tuple(inputs)
         # [batch, H, W] -> [batch, 1, H, W]
-        target = target.unsqueeze(1)
-        target_sub4 = F.interpolate(target, pred_sub4.size()[2:], mode='bilinear', align_corners=True).squeeze(1)
-        target_sub8 = F.interpolate(target, pred_sub8.size()[2:], mode='bilinear', align_corners=True).squeeze(1)
+        target = target.unsqueeze(1).float()
+        target_sub4 = F.interpolate(target, pred_sub4.size()[2:], mode='bilinear', align_corners=True).squeeze(1).long()
+        target_sub8 = F.interpolate(target, pred_sub8.size()[2:], mode='bilinear', align_corners=True).squeeze(1).long()
         target_sub16 = F.interpolate(target, pred_sub16.size()[2:], mode='bilinear', align_corners=True).squeeze(
-            1)
-
-        loss1 = super(ICNetLoss, self).forward(pred_sub4.squeeze_(1), target_sub4)
-        loss2 = super(ICNetLoss, self).forward(pred_sub8.squeeze_(1), target_sub8)
-        loss3 = super(ICNetLoss, self).forward(pred_sub16.squeeze_(1), target_sub16)
+            1).long()
+        loss1 = super(ICNetLoss, self).forward(pred_sub4, target_sub4)
+        loss2 = super(ICNetLoss, self).forward(pred_sub8, target_sub8)
+        loss3 = super(ICNetLoss, self).forward(pred_sub16, target_sub16)
         # return dict(loss=loss1 + loss2 * self.aux_weight + loss3 * self.aux_weight)
         return loss1 + loss2 * self.aux_weight + loss3 * self.aux_weight
