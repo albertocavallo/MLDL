d3374289d6df4d73145c65539d615f139821518e
albertocavallo <75563684+albertocavallo@users.noreply.github.com>
2023-05-21 15:53:49 +0200

    Update data_augmentation.py

--- data_augmentation.py
+++ data_augmentation.py
@@ -2,44 +2,47 @@ import os
 from imgaug import augmenters as iaa
 import cv2
 
-def augment_images(input_folder, output_folder, augmentation_factor):
-    # Create output folder if it doesn't exist
+def augment_images(input_folder, output_folder, input_folder_l, output_folder_l, augmentation_factor):
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
-
-    # Define augmentation sequence
     seq = iaa.Sequential([
         iaa.Fliplr(0.5),  # horizontally flip 50% of images
         iaa.GaussianBlur(sigma=(0, 1.0)),  # apply Gaussian blur with sigma between 0 and 1.0
-        iaa.Affine(rotate=(-45, 45))  # rotate images between -45 and 45 degrees
-        # Add more augmentation techniques as needed
+        iaa.Affine(rotate=(0, 90))  # rotate images between -45 and 45 degrees
     ])
 
-    # Iterate over input folder
     for filename in os.listdir(input_folder):
-        if filename.endswith('.jpg') or filename.endswith('.png'):  # consider only image files
+        if filename.endswith('.jpg') or filename.endswith('.png'):  
             input_path = os.path.join(input_folder, filename)
-            output_path = os.path.join(output_folder, filename.split('.')[0])  # remove file extension
-
-            # Read the image
+            output_path = os.path.join(output_folder, filename.split('.')[0])
             image = cv2.imread(input_path)
-
-            # Apply data augmentation multiple times
             for i in range(augmentation_factor):
                 augmented_image = seq.augment_image(image)
-
-                # Save augmented image
                 output_filename = f'{output_path}_augmented_{i}.jpg'
                 cv2.imwrite(output_filename, augmented_image)
 
                 print(f'Saved augmented image: {output_filename}')
 
-    print('Data augmentation completed.')
+    print('Data augmentation, train images completed.')
+
+    for filename in os.listdir(input_folder_l):
+        if filename.endswith('.jpg') or filename.endswith('.png'): 
+            input_path = os.path.join(input_folder_l, filename)
+            output_path = os.path.join(output_folder_l, filename.split('.')[0])  
+
+            image = cv2.imread(input_path)
+
+            for i in range(augmentation_factor):
+                output_filename = f'{output_path}_augmented_{i}.jpg'
+                cv2.imwrite(output_filename, image)
+
+    print('Data augmentation, train labels completed.')
 
 
-# Example usage
 input_folder = '/content/drive/MyDrive/dataset/train'
 output_folder = '/content/drive/MyDrive/dataset/train'
-augmentation_factor = 1  # Generate 5 augmented images for each input image
+input_folder_l = '/content/drive/MyDrive/dataset/labels/train'
+output_folder_l = '/content/drive/MyDrive/dataset/labels/train'
+augmentation_factor = 1  # Generate augmentation_factor images for each input image
 
-augment_images(input_folder, output_folder, augmentation_factor)
+augment_images(input_folder, output_folder,input_folder_l, output_folder_l, augmentation_factor)
